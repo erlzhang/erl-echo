b6ab1d4a9b4ad8cb6c4879b4d95f85603d51a92d
Sharon Zhang <zhangshiyu1992@hotmail.com>
2024-02-22 19:32:08 +0800

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,14 @@
+"use client"
+
 import Image from 'next/image'
 import Link from 'next/link';
+import { Button } from './ui/form';
+import { useContext } from 'react';
+import { AuthContext } from '@/contexts/authContext';
 
 export default function() {
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <header className="header">
         <div className="logo">
@@ -9,7 +16,16 @@ export default function() {
             <Image src="/quill-pen.svg" alt="logo" width="40" height="40" />
             Echo
           </Link>
-        </div>      
+        </div>
+        <div className="user-info">
+          Hello, {user.name}!
+          <Button
+            onClick={logout}
+            style={{
+              textDecoration: 'underline'
+            }}
+          >LogOut</Button>
+        </div>
     </header>
   )
 }
